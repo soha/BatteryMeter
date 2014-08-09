364deca02f95a24197b12202fb9fbeb411b92baa
sohatach <sohatach+git@gmail.com>
2014-08-09 16:02:38 +0900

    refs #150 サーバにログファイルアップロード中は、ステータスバーに通知する。

--- app/src/main/java/s/tokyo/jp/batterymeter/MongoLogPost.java
+++ app/src/main/java/s/tokyo/jp/batterymeter/MongoLogPost.java
@@ -1,6 +1,11 @@
 package s.tokyo.jp.batterymeter;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Environment;
@@ -31,6 +36,8 @@ public class MongoLogPost extends AsyncTask<Uri.Builder, Void, String> {
     private final static String SDFILE = LOGDIR + "/" + "btlog.csv";
     private static boolean enable = true;
 
+    private NotificationManager nm;
+
     private Activity myActivity;
 
     private StringBuilder postData;
@@ -43,6 +50,22 @@ public class MongoLogPost extends AsyncTask<Uri.Builder, Void, String> {
 
     @Override
     protected String doInBackground(Uri.Builder... builder) {
+
+        nm = (NotificationManager)myActivity.getSystemService(Context.NOTIFICATION_SERVICE);
+        String ticker="アップロード通知";
+        String title = "BatteryMeter";
+        String message = "ログファイルアップロード中";
+        Notification notification = new Notification(android.R.drawable.ic_menu_info_details,ticker,System.currentTimeMillis());
+        PendingIntent intent = PendingIntent.getActivity(myActivity, 0, new Intent(myActivity, s.tokyo.jp.batterymeter.MyActivity.class), 0);
+        notification.setLatestEventInfo(myActivity.getApplicationContext(), title, message, intent);
+        // 同じIDで表示中の通知を消す
+        nm.cancel(0);
+
+        // 動作フラグを書き換える(常駐)
+        notification.flags = Notification.FLAG_ONGOING_EVENT;
+
+        // Notificationの表示
+        nm.notify(0,notification);
 
         postData.setLength(0);
 
@@ -119,5 +142,8 @@ public class MongoLogPost extends AsyncTask<Uri.Builder, Void, String> {
     protected void onPostExecute(String result) {
         Toast.makeText(this.myActivity, "ログファイルをアップロードしました。", Toast.LENGTH_SHORT).show();
         Toast.makeText(this.myActivity, postData.toString(), Toast.LENGTH_LONG).show();
+
+        // 同じIDで表示中の通知を消す
+        nm.cancel(0);
     }
 }
